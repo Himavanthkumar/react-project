21c27e7eb25f16d50316d7cd47da62ff19abe3de
Himavanth Kumar <Himavanthking140@gmail.com>
2025-03-15 00:20:35 -0500

    Nothing

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
-  let count1 = 0;
-  const [count, setCount] = useState(0);
-  function handleClick() {
-    count1 = count1 + 1;
-    console.log(count1);
-  }
-
-  return (
-    <>
-      <h1>Without useState, I am not updated {count1}</h1>
-      <button onClick={handleClick}>Click me!</button>
-      <h1>With useState, I am updated {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Click me!</button>
-      {/* best practice is to use setCount((count) => count + 1)
-      Java script uses batching under the hood so if we use the 
-      setCount(count + 1)
-      setCount(count + 1)
-      setCount(count + 1)
-      setCount(count + 1)
-
-      All the above instructions are processed at the same time so to avoid this use
-      setCount((count) => count + 1)
-      
-      */}
-      <button
-        onClick={() => {
-          setCount((count) => count + 1);
-          setCount((count) => count + 1);
-          setCount((count) => count + 1);
-          setCount((count) => count + 1);
-        }}
-      >
-        Click me to add 4!
-      </button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
